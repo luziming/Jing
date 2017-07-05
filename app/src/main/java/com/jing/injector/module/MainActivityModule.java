253d4f85042ffe9fcf68aa48fdb69be3d26b771e
Jake Lu <geniuskai92@gmail.com>
2017-07-05 20:37:37 +0800

    测一哈

---
+++ app/src/main/java/com/jing/injector/module/MainActivityModule.java
@@ -0,0 +1,14 @@
+package com.jing.injector.module;
+
+import dagger.Module;
+
+/**
+ * 写代码救不了中国人
+ * ---------------------------------
+ * Created by luziming on 2017/7/5.
+ */
+
+@Module
+public class MainActivityModule {
+
+}
